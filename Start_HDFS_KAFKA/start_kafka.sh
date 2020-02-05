0133e61720eeb8b48f4a59bc88f4d6b4b4992ad1
Big Data Team <jeff@alkire.net>
2020-02-05 13:53:12 -0500

    changes to start kafka

--- Start_HDFS_KAFKA/start_kafka.sh
+++ Start_HDFS_KAFKA/start_kafka.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
+source ~/.bash_profile
 
 cd
 
-zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties
+cd /opt/kafka/config
+echo "
+listeners=PLAINTEXT://:9091
+sed -e "s/log.dirs=\/tmp\/kafka-logs/log.dirs=\/tmp\/kafka-logs-1/
+" >> server.properties.1
 
-kafka-server-start.sh $KAFKA_HOME/config/server.properties
+echo "
+listeners=PLAINTEXT://:9092
+sed -e "s/log.dirs=\/tmp\/kafka-logs/log.dirs=\/tmp\/kafka-logs-2/
+" >> server.properties.2
+
+nohup zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties &
+
+nohup kafka-server-start.sh $KAFKA_HOME/config/server.properties.1 &
+nohup kafka-server-start.sh $KAFKA_HOME/config/server.properties.2 &
 
 

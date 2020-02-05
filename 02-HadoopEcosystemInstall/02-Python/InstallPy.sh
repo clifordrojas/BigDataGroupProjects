bb4eeef6952df8d827e2952f2ab81282dcf8f3a6
alkirej <jeff@alkire.net>
2020-02-04 20:25:52 -0500

    add step 2 python to install script

--- 02-HadoopEcosystemInstall/02-Python/InstallPy.sh
+++ 02-HadoopEcosystemInstall/02-Python/InstallPy.sh
@@ -2,17 +2,14 @@
 cd /opt
 
 #download
-wget https://www.python.org/ftp/python/3.6.9/Python-3.6.9.tgz
+sudo wget https://www.python.org/ftp/python/3.6.9/Python-3.6.9.tgz
 
-#unzip
+sudo chmod 755 /opt/
+
+# unzip
 tar -xf Python-3.6.9.tgz
 rm Python-3.6.9.tgz
 
-sudo chmod 777 /opt/
+sudo chmod 755 /opt/
+sudo ln -s /opt/python-3.6.9/bin/ python
 
-echo "
-export PYTHON_HOME=/opt/Python-3.6.9
-export PATH=$PATH:/opt/Python-3.6.9/bin
-" >> .bash_profile
-
-source ~/.bash_profile
--- 02-HadoopEcosystemInstall/install-all.sh
+++ 02-HadoopEcosystemInstall/install-all.sh
@@ -1,10 +1,18 @@
 # begin
+echo ... Installing Java ...
 01-Java/installJava.sh
-export JAVA_HOME=/opt/jdk1.8.0_221
-export PATH=$JAVA_HOME/bin
 
-#02-Python/installPY.sh
+export JAVA_HOME=/opt/jdk1.8.0_221
+export PATH=$JAVA_HOME/bin:$PATH
+
+echo ... Installing Python ...
+02-Python/InstallPy.sh
+export PYTHON_HOME=/opt/Python-3.6.9
+export PATH=$PATH:/opt/Python-3.6.9/bin
+
+echo ... Installing Scala ...
 #!!!03-Scala/run.sh
+
 #!!!04-Sbt/run.sh
 #05-OpenSSH/openssh.sh
 #06-Git/git_install.sh
@@ -17,7 +25,9 @@ export PATH=$JAVA_HOME/bin
 
 cd ~
 echo "
+export PYTHON_HOME=/opt/Python-3.6.9
+
 export JAVA_HOME=$JAVA_HOME
-export PATH=$JAVA_HOME/bin:$PATH
+export PATH=$JAVA_HOME/bin:$PYTHON_HOME/bin:$PATH
 " > ~/.bash_profile
 

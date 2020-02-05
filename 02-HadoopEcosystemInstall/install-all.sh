281f79b5138eaf81906ae01602bb5d437bcbe953
Big Data Team <jeff@alkire.net>
2020-02-04 23:00:01 -0500

    install all script updates

--- 02-HadoopEcosystemInstall/install-all.sh
+++ 02-HadoopEcosystemInstall/install-all.sh
@@ -1,27 +1,55 @@
 # begin
+LF=~/hadoop_ecosystem_install.log
+
 echo ... Installing Java ...
 01-Java/installJava.sh > ~/hadoop_ecosystem_install.log
-
 export JAVA_HOME=/opt/jdk1.8.0_221
 export PATH=$JAVA_HOME/bin:$PATH
+echo
 
 echo ... Installing Python ...
 02-Python/InstallPy.sh
 export PYTHON_HOME=/opt/Python-3.6.9
 export PATH=$PATH:/opt/Python-3.6.9/bin
+echo
 
 echo ... Installing Scala ...
 #!!!03-Scala/run.sh
+echo
 
+echo ... Installing Sbt ...
 #!!!04-Sbt/run.sh
-05-OpenSSH/openssh.sh
-06-Github/git_install.sh
+echo
 
+echo ... Installing OpenSSH ...
+05-OpenSSH/openssh.sh
+echo
+
+echo ... Installing Github ...
+06-GitHub/git_install.sh
+echo
+
+echo ... Installing Netcat ... 
 #!!!07-Netcat/run.sh
+echo
+
+echo ... Installing Hadoop ...
 #08-Hadoop/hadoop_install.sh
+echo
+
+echo ... Installing Kafka ...
 #09-Kafka/installKafka.sh
+echo
+
+echo ... Installing Airflow ...
 #!!!10-Airflow/run.sh
+echo
+
+echo ... Installing Spark ...
 #11-Spark/InstallSpark.sh
+echo
+
+echo ... Setting up Pyspark ...
 #!!!12-PySpark/run.sh
 
 cd ~

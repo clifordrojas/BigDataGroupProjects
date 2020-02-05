85a72c8ca3a7f45b2145da101a4e07f566bb3fcc
aligas1 <alaligas@gmail.com>
2020-02-04 20:41:11 -0500

    Create airflow.sh

---
+++ 01-HadoopInstall/airflow.sh
@@ -0,0 +1,12 @@
+#pip install 
+pip3 install apache-airflow
+
+sudo chmod 777 /opt
+
+nano .bash_profile
+
+#set path
+echo " export AIRFLOW_HOME=/home/alicja/airflow
+" >> .bash_profile
+
+source ~/.bash_profile
